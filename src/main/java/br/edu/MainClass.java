ddbc23596880aa18346061f71a27cd2d36163d46
“Carolina <cfsm1@discente.ifpe.edu.br>
2025-02-24 17:57:00 -0300

    clone testando

--- src/main/java/br/edu/MainClass.java
+++ src/main/java/br/edu/MainClass.java
@@ -2,6 +2,6 @@ package br.edu;
 
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Rodando");
+        System.out.println("Atualizando com o clone rs");
     }
 }
