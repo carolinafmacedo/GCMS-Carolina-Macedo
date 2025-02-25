3586eac55de12f801299554205afce65b80b1f43
“Carolina <cfsm1@discente.ifpe.edu.br>
2025-02-25 10:38:40 -0300

    erros de java ajustados

--- src/main/java/br/edu/MainClass.java
+++ src/main/java/br/edu/MainClass.java
@@ -2,10 +2,6 @@ package br.edu;
 
 public class MainClass {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("agora a original novamente!!!");
-=======
-        System.out.println("Atualizando com o clone rs");
->>>>>>> ddbc23596880aa18346061f71a27cd2d36163d46
     }
 }
